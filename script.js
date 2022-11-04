e83827bb587e69c5aec710b95c2b2fe69a9daa13
Lien Sanchez <liensilviosanchez@gmail.com>
2022-11-04 20:01:09 -0300

    cambios en array y en botones de calculadora hace calculos simples

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let preArray =[]
 //array donde se van a guardar los numeros para el calculo
 let array = []
 
-
-//botones
+//botones numericos
 document.getElementById('cero').onclick = function () {
   let valorTecla = 0
   preArray.push(parseInt(valorTecla))
@@ -19,27 +18,75 @@ document.getElementById('uno').onclick = function () {
   preArray.push(parseInt(valorTecla))
   console.log(preArray)
 }
-
+document.getElementById('dos').onclick = function () {
+  let valorTecla = 2
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('tres').onclick = function () {
+  let valorTecla = 3
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('cuatro').onclick = function () {
+  let valorTecla = 4
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('cinco').onclick = function () {
+  let valorTecla = 5
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('seis').onclick = function () {
+  let valorTecla = 6
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('siete').onclick = function () {
+  let valorTecla = 7
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('ocho').onclick = function () {
+  let valorTecla = 8
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
+document.getElementById('nueve').onclick = function () {
+  let valorTecla = 9
+  preArray.push(parseInt(valorTecla))
+  console.log(preArray)
+}
 
 
 //los botones van a agregar los numeros al array para las operaciones y van a cambiar a true la operacion que se quiera realzar
 document.getElementById('suma').onclick = function (){
   suma = true
+  let numeroSegundo = preArray.join('')
+  array.push(parseInt(numeroSegundo))
+  preArray=[]
+  console.log(array)
 }
 document.getElementById('resta').onclick = function () {
-  let numeroResta = document.getElementById('ingresoNum').value
-  array.push(parseInt(numeroResta))
   resta = true
+  let numeroSegundo = preArray.join('')
+  array.push(parseInt(numeroSegundo))
+  preArray = []
+  console.log(array)
 }
 document.getElementById('multiplicacion').onclick = function () {
-  let numeroMultiplicacion = document.getElementById('ingresoNum').value
-  array.push(parseInt(numeroMultiplicacion))
   multiplicacion = true
+  let numeroSegundo = preArray.join('')
+  array.push(parseInt(numeroSegundo))
+  preArray = []
 }
 document.getElementById('division').onclick = function () {
-  let numeroDivision = document.getElementById('ingresoNum').value
-  array.push(parseInt(numeroDivision))
-  division = true
+  division = true 
+  let numeroDividor = preArray.join('')
+  array.push(parseInt(numeroDividor))
+  preArray = []
+  
 }
 
 
@@ -47,21 +94,27 @@ document.getElementById('division').onclick = function () {
 document.getElementById('igual').onclick = function(){
   let numeroSegundo = preArray.join('')
   array.push(parseInt(numeroSegundo))
+  preArray=[]  
   if(suma == true){
     array.reduce((primero, segundo) => {
       console.log(primero + segundo)
     })
+    suma = false
   }else if (resta == true){
     array.reduce((primero, segundo) => {
       console.log(primero - segundo)
     })
+    resta = false
   }else if (multiplicacion == true){
     array.reduce((primero, segundo) => {
       console.log(primero * segundo)
     })
+    multiplicacion = false
   }else if (division == true){
     array.reduce((primero, segundo) => {
       console.log(primero / segundo)
     })
+    division = false
   }
+  array=[]
 }
